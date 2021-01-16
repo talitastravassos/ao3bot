2a771f043e1657bd67f01ae872b680f018df32b6
talitast <talita.travassos@projuris.com.br>
2021-01-16 11:31:40 -0300

    init config bot

---
+++ bot.js
@@ -0,0 +1,8 @@
+const twit = require("twit");
+const config = require("./config");
+
+require("dotenv").config();
+
+console.log(config);
+
+console.log(process.env.ACCESS_TOKEN);
---
+++ config.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+module.exports = {
+  consumer_key: process.env.APPLICATION_CONSUMER_KEY,
+  consumer_secret: process.env.APPLICATION_CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+};
